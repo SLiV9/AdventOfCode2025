04700acb1fa9f4e4bd69cd492ff98203e13595ff
Sander in 't Veld <sveld@block-tech.io>
2025-12-02 22:16:16 +0100

    Day 2, part two in 3.5us

--- src/day2.rs
+++ src/day2.rs
@@ -124,9 +124,90 @@ pub fn part2(input: &str) -> u64
 
 fn assess_part2(start: Id, end: Id) -> u64
 {
-	// if len is 8, don't count 2's because they are already counted as 4's
-	// only count 1's if len is prime
-	todo!()
+	if start.len == end.len
+	{
+		assess_part2_(start, end)
+	}
+	else
+	{
+		debug_assert_eq!(start.len + 1, end.len);
+		let mid = 10u64.pow(start.len);
+		let mid0 = Id {
+			value: mid - 1,
+			len: start.len,
+		};
+		let mid1 = Id {
+			value: mid,
+			len: end.len,
+		};
+		assess_part2_(start, mid0) + assess_part2_(mid1, end)
+	}
+}
+
+const PRIMES: [u32; 8] = [2, 3, 5, 7, 11, 13, 17, 19];
+const DOUBLETS: [u32; 4] = [6, 10, 14, 15];
+
+fn assess_part2_(start: Id, end: Id) -> u64
+{
+	let sum_of_invalids: u64 = PRIMES
+		.iter()
+		.copied()
+		.map(|prime| assess_divisor(start, end, prime))
+		.sum();
+	let sum_of_double_counted: u64 = DOUBLETS
+		.iter()
+		.copied()
+		.map(|divisor| assess_divisor(start, end, divisor))
+		.sum();
+	sum_of_invalids - sum_of_double_counted
+}
+
+fn assess_divisor(start: Id, end: Id, divisor: u32) -> u64
+{
+	if start.len % divisor != 0
+	{
+		return 0;
+	}
+	let mut sum_of_invalids = 0;
+
+	let num_digits_per_chunk = start.len / divisor;
+
+	let build_invalid_id = |chunk| {
+		let mut invalid_id = chunk;
+		for _ in 1..divisor
+		{
+			invalid_id *= 10u64.pow(num_digits_per_chunk);
+			invalid_id += chunk;
+		}
+		invalid_id
+	};
+
+	let chunksize = 10u64.pow(start.len - num_digits_per_chunk);
+	let start_in_chunks = start.value / chunksize;
+	let end_in_chunks = end.value / chunksize;
+
+	for chunk in (start_in_chunks + 1)..end_in_chunks
+	{
+		let invalid_id = build_invalid_id(chunk);
+		sum_of_invalids += invalid_id;
+	}
+
+	if end_in_chunks > start_in_chunks
+	{
+		let invalid_id = build_invalid_id(end_in_chunks);
+		if invalid_id <= end.value
+		{
+			sum_of_invalids += invalid_id;
+		}
+	}
+
+	let invalid_id = build_invalid_id(start_in_chunks);
+	if (start.value..=end.value).contains(&invalid_id)
+	{
+		sum_of_invalids += invalid_id;
+	}
+
+	sum_of_invalids
 }
 
 #[cfg(test)]
@@ -137,16 +218,30 @@ mod tests
 	use super::*;
 
 	#[test]
-	fn test_day1_part1_given()
+	fn test_part1_given()
 	{
 		let given = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 		assert_eq!(part1(given), 1227775554);
 	}
 
 	#[test]
-	fn test_day1_part2_given()
+	fn test_part2_given()
 	{
 		let given = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
-		assert_eq!(part2(given), 6);
+		assert_eq!(part2(given), 4174379265);
+	}
+
+	#[test]
+	fn test_part2_duplicates()
+	{
+		let mut id = 22;
+		for _ in 0..15
+		{
+			let input = format!("{id}-{id}");
+			assert_eq!(part2(&input), id);
+
+			id *= 10;
+			id += 2;
+		}
 	}
 }
