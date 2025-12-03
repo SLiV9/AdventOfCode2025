2166e6ea47c67ea492d9284d814d504a1e5f55f6
Sander in 't Veld <sveld@block-tech.io>
2025-12-03 10:01:38 +0100

    Day 3, part two in 75us

--- src/day3.rs
+++ src/day3.rs
@@ -4,17 +4,21 @@ pub fn part1(input: &str) -> u64
 	input
 		.lines()
 		.filter(|x| !x.is_empty())
-		.map(find_largest_possible_joltage_in_bank)
+		.map(find_largest_safe_joltage_in_bank)
 		.sum()
 }
 
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> u64
 {
-	input.len() as u64
+	input
+		.lines()
+		.filter(|x| !x.is_empty())
+		.map(find_largest_overcharged_joltage_in_bank)
+		.sum()
 }
 
-fn find_largest_possible_joltage_in_bank(bank: &str) -> u64
+fn find_largest_safe_joltage_in_bank(bank: &str) -> u64
 {
 	let digits = bank.as_bytes();
 	assert!(digits.len() >= 2);
@@ -32,6 +36,33 @@ fn find_largest_possible_joltage_in_bank(bank: &str) -> u64
 	debug_assert!(start + 1 < digits.len());
 	let second_digit = digits[(start + 1)..].iter().copied().max().unwrap();
 	u64::from(first_digit - b'0') * 10 + u64::from(second_digit - b'0')
+}
+
+fn find_largest_overcharged_joltage_in_bank(bank: &str) -> u64
+{
+	let digits = bank.as_bytes();
+	let mut leeway = digits.len() - 12;
+	let mut joltage = 0;
+	let mut start = 0;
+	for _ in 0..12
+	{
+		let mut best_digit = digits[start];
+		let mut digits_skipped = 0;
+		for skip in 1..=leeway
+		{
+			let i = start + skip;
+			if digits[i] > best_digit
+			{
+				best_digit = digits[i];
+				digits_skipped = skip;
+			}
+		}
+		start += 1 + digits_skipped;
+		leeway -= digits_skipped;
+		joltage *= 10;
+		joltage += u64::from(best_digit - b'0');
+	}
+	joltage
 }
 
 #[cfg(test)]
@@ -58,7 +89,7 @@ mod tests
 811111111111119
 234234234234278
 818181911112111";
-		assert_eq!(part2(given), 0);
+		assert_eq!(part2(given), 3121910778619);
 	}
 
 	#[test]
@@ -67,5 +98,12 @@ mod tests
 		assert_eq!(part1("12"), 12);
 		assert_eq!(part1("123"), 23);
 		assert_eq!(part1("4341"), 44);
+	}
+
+	#[test]
+	fn test_part2_testcases()
+	{
+		assert_eq!(part2("234234234234278"), 434234234278);
+		assert_eq!(part2("281111111111119"), 811111111119);
 	}
 }
