d7597d4505c463b7e86e2865b0aba29821ac00f1
Sander in 't Veld <sveld@block-tech.io>
2025-12-03 22:12:58 +0100

    Day 3, wrote a byte-by-byte solver, not yet faster
    The assembly is super small and readable, but is not autovectorized

--- src/day3.rs
+++ src/day3.rs
@@ -1,6 +1,11 @@
 #[aoc(day3, part1)]
-pub fn part1(input: &str) -> u64
-{
+pub fn part1(input: &str) -> u64 {
+	let solution = Solver::<2>::solve(input);
+	debug_assert_eq!(solution, part1_clean(input));
+	solution
+}
+
+fn part1_clean(input: &str) -> u64 {
 	input
 		.lines()
 		.filter(|x| !x.is_empty())
@@ -9,8 +14,13 @@ pub fn part1(input: &str) -> u64
 }
 
 #[aoc(day3, part2)]
-pub fn part2(input: &str) -> u64
-{
+pub fn part2(input: &str) -> u64 {
+	let solution = Solver::<12>::solve(input);
+	debug_assert_eq!(solution, part2_clean(input));
+	solution
+}
+
+fn part2_clean(input: &str) -> u64 {
 	input
 		.lines()
 		.filter(|x| !x.is_empty())
@@ -18,17 +28,14 @@ pub fn part2(input: &str) -> u64
 		.sum()
 }
 
-fn find_largest_safe_joltage_in_bank(bank: &str) -> u64
-{
+fn find_largest_safe_joltage_in_bank(bank: &str) -> u64 {
 	let digits = bank.as_bytes();
 	assert!(digits.len() >= 2);
 	debug_assert!(digits.iter().all(|x| x.is_ascii_digit()));
 	let mut start = 0;
 	let mut first_digit = digits[0];
-	for i in 1..(digits.len() - 1)
-	{
-		if digits[i] > first_digit
-		{
+	for i in 1..(digits.len() - 1) {
+		if digits[i] > first_digit {
 			first_digit = digits[i];
 			start = i;
 		}
@@ -38,21 +45,17 @@ fn find_largest_safe_joltage_in_bank(bank: &str) -> u64
 	u64::from(first_digit - b'0') * 10 + u64::from(second_digit - b'0')
 }
 
-fn find_largest_overcharged_joltage_in_bank(bank: &str) -> u64
-{
+fn find_largest_overcharged_joltage_in_bank(bank: &str) -> u64 {
 	let digits = bank.as_bytes();
 	let mut leeway = digits.len() - 12;
 	let mut joltage = 0;
 	let mut start = 0;
-	for _ in 0..12
-	{
+	for _ in 0..12 {
 		let mut best_digit = digits[start];
 		let mut digits_skipped = 0;
-		for skip in 1..=leeway
-		{
+		for skip in 1..=leeway {
 			let i = start + skip;
-			if digits[i] > best_digit
-			{
+			if digits[i] > best_digit {
 				best_digit = digits[i];
 				digits_skipped = skip;
 			}
@@ -65,16 +68,50 @@ fn find_largest_overcharged_joltage_in_bank(bank: &str) -> u64
 	joltage
 }
 
+#[derive(Clone, Copy, Debug)]
+struct Solver<const N: usize> {
+	row: [u64; N],
+	sum: u64,
+}
+
+impl<const N: usize> Solver<N> {
+	fn solve(input: &str) -> u64 {
+		let mut solver = Solver {
+			row: [0; N],
+			sum: 0,
+		};
+		for x in input.bytes() {
+			solver.process(x);
+		}
+		solver.finalize()
+	}
+
+	fn process(&mut self, ascii_digit_or_whitespace: u8) {
+		let Some(digit_value) = ascii_digit_or_whitespace.checked_sub(b'0') else {
+			self.sum += self.row[0];
+			self.row = [0; N];
+			return;
+		};
+		let digit_value = u64::from(digit_value);
+		for i in 0..(N - 1) {
+			self.row[i] = self.row[i].max(self.row[i + 1] * 10 + digit_value);
+		}
+		self.row[N - 1] = self.row[N - 1].max(digit_value);
+	}
+
+	fn finalize(self) -> u64 {
+		self.sum + self.row[0]
+	}
+}
+
 #[cfg(test)]
-mod tests
-{
+mod tests {
 	use pretty_assertions::assert_eq;
 
 	use super::*;
 
 	#[test]
-	fn test_part1_given()
-	{
+	fn test_part1_given() {
 		let given = "987654321111111
 811111111111119
 234234234234278
@@ -83,8 +120,7 @@ mod tests
 	}
 
 	#[test]
-	fn test_part2_given()
-	{
+	fn test_part2_given() {
 		let given = "987654321111111
 811111111111119
 234234234234278
@@ -93,17 +129,19 @@ mod tests
 	}
 
 	#[test]
-	fn test_part1_testcases()
-	{
+	fn test_part1_testcases() {
 		assert_eq!(part1("12"), 12);
 		assert_eq!(part1("123"), 23);
 		assert_eq!(part1("4341"), 44);
 	}
 
 	#[test]
-	fn test_part2_testcases()
-	{
+	fn test_part2_testcases() {
 		assert_eq!(part2("234234234234278"), 434234234278);
 		assert_eq!(part2("281111111111119"), 811111111119);
+		assert_eq!(part2("281111811111119"), 811811111119);
+		assert_eq!(part2("282222811111119"), 822811111119);
+		assert_eq!(part2("282342811111119"), 842811111119);
+		assert_eq!(part2("822942811111119"), 942811111119);
 	}
 }
