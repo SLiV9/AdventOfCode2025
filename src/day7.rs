d039cd61fe1a9cfe519170227e719dc51bf4598b
Sander in 't Veld <sveld@block-tech.io>
2025-12-07 14:01:36 +0100

    Day 7, part one in 19us

--- src/day7.rs
+++ src/day7.rs
@@ -1,27 +1,28 @@
 #[aoc(day7, part1)]
 pub fn part1(input: &str) -> u64 {
-	let mut bitmask = [false; 1024];
+	let mut has_beam = [0u8; 1024];
 	let mut number_of_splits = 0;
 	let mut lines = input.lines();
-	let first_line = lines.next().unwrap();
+	let first_line = lines.next().expect("non-empty input");
 	let first_line = first_line.as_bytes();
 	// Adding one to simplify logic, and rounding up to the nearest multiple of 8.
 	let n = (first_line.len() / 8 + 1) * 8;
-	let bitmask = &mut bitmask[..n];
-	let start_pos = first_line.iter().copied().position(|x| x == b'S').unwrap();
-	bitmask[start_pos] = true;
+	let has_beam = &mut has_beam[..n];
+	let start_pos = first_line.iter().copied().position(|x| x == b'S');
+	let start_pos = start_pos.expect("start at S");
+	has_beam[start_pos] = 1;
 	for line in lines {
 		let line = line.as_bytes();
 		let k = n.min(line.len());
-		let mut delayed_store = false;
+		let mut delayed_store = 0;
 		for i in 1..k {
 			let is_splitter = line[i] == b'^';
-			let is_active = bitmask[i];
-			let is_split: bool = is_splitter & is_active;
+			let split_mask = if is_splitter { u8::MAX } else { 0 };
+			let is_split: u8 = has_beam[i] & split_mask;
 			number_of_splits += u64::from(is_split);
-			bitmask[i - 1] |= is_split;
-			bitmask[i] &= !is_split;
-			bitmask[i] |= delayed_store;
+			has_beam[i - 1] |= is_split;
+			has_beam[i] &= !is_split;
+			has_beam[i] |= delayed_store;
 			delayed_store = is_split;
 		}
 	}
@@ -32,12 +33,13 @@ pub fn part1(input: &str) -> u64 {
 pub fn part2(input: &str) -> u64 {
 	let mut timelines = [0u64; 1024];
 	let mut lines = input.lines();
-	let first_line = lines.next().unwrap();
+	let first_line = lines.next().expect("non-empty input");
 	let first_line = first_line.as_bytes();
 	// Adding one to simplify logic, and rounding up to the nearest multiple of 8.
 	let n = (first_line.len() / 8 + 1) * 8;
 	let timelines = &mut timelines[..n];
-	let start_pos = first_line.iter().copied().position(|x| x == b'S').unwrap();
+	let start_pos = first_line.iter().copied().position(|x| x == b'S');
+	let start_pos = start_pos.expect("start at S");
 	timelines[start_pos] = 1;
 	for line in lines {
 		let line = line.as_bytes();
