f7febca0a45ec01691c4ee30d1e07f7a3225e660
Sander in 't Veld <sveld@block-tech.io>
2025-12-09 22:00:27 +0100

    Day 9, part two in 11.5ms

--- src/day9.rs
+++ src/day9.rs
@@ -101,11 +101,11 @@ fn rect_intersects_line(
 ) -> bool {
 	if x2 == x3 {
 		// Vertical line.
-		x0 < x2 && x2 < x1 && y3 >= y0 && y2 <= y1
+		x0 < x2 && x2 < x1 && y3 > y0 && y2 < y1
 	} else {
 		// Horizontal line.
 		debug_assert_eq!(y2, y3);
-		y0 < y2 && y2 < y1 && x3 >= x0 && x2 <= x1
+		y0 < y2 && y2 < y1 && x3 > x0 && x2 < x1
 	}
 }
 
