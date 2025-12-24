77ea15df52d7a5423b0580cc03a20bbb856a37b1
Sander in 't Veld <sveld@block-tech.io>
2025-12-24 15:59:11 +0100

    Thinking about Day 10, part two
    I have an interesting solution, but need to figure out how to brute force it, or recall some linear algebra

--- src/day10.rs
+++ src/day10.rs
@@ -7,7 +7,7 @@ pub fn part1(input: &str) -> usize {
 
 #[aoc(day10, part2)]
 pub fn part2(input: &str) -> usize {
-	input.len()
+	input.lines().map(str::trim).map(assess_part2).sum()
 }
 
 fn assess_part1(line: &str) -> usize {
@@ -16,62 +16,22 @@ fn assess_part1(line: &str) -> usize {
 	{
 		let bytes = line.as_bytes();
 		let mut i = 1;
-		let mut target_setter = 1;
-		loop {
-			match bytes[i] {
-				b'#' => {
-					target |= target_setter;
-					target_setter <<= 1;
-				}
-				b'.' => {
-					target_setter <<= 1;
-				}
-				b']' => {
-					i += 3;
-					break;
-				}
-				_ => (),
-			}
-			i += 1;
-		}
-		let mut button = 0;
-		let mut number = 0;
-		loop {
-			let b = bytes[i];
-			i += 1;
-			match b {
-				b'0'..=b'9' => {
-					number *= 10;
-					number += u32::from(b - b'0');
-				}
-				b',' => {
-					button |= 0b1 << number;
-					number = 0;
-				}
-				b')' => {
-					button |= 0b1 << number;
-					number = 0;
-					buttons.push(button);
-					button = 0;
-				}
-				b'{' => break,
-				_ => continue,
-			}
-		}
+		extract_target(&mut target, bytes, &mut i);
+		extract_buttons(&mut buttons, bytes, &mut i);
 	}
 
 	let target: u32 = target;
 	let buttons: &[u32] = &buttons[..];
 
-	// dbg!(format!("{target:08b}"));
-	// for &button in buttons {
-	// 	dbg!(format!("{button:08b}"));
-	// }
+	debug_print("target", target);
+	for &button in buttons {
+		debug_print("button", button);
+	}
 
 	let mut min_presses = u32::MAX;
 	let num_possibilities = 2u32 << buttons.len();
 	for possibility in 0..num_possibilities {
-		// dbg!(format!("{possibility:016b}"));
+		debug_print("possibility", possibility);
 		let num_pressed = possibility.count_ones();
 		if num_pressed < min_presses {
 			let mut result = 0;
@@ -82,13 +42,165 @@ fn assess_part1(line: &str) -> usize {
 				}
 				possibility_bits >>= 1;
 			}
-			// dbg!(format!("{result:08b}"));
+			debug_print("result", result);
 			if result == target {
 				min_presses = num_pressed;
 			}
 		}
 	}
 	min_presses as usize
+}
+
+fn assess_part2(line: &str) -> usize {
+	let mut buttons: SmallVec<[u32; 16]> = SmallVec::new();
+	let mut joltage: SmallVec<[u32; 16]> = SmallVec::new();
+	{
+		let bytes = line.as_bytes();
+		let mut i = 1;
+		let mut target = 0;
+		extract_target(&mut target, bytes, &mut i);
+		extract_buttons(&mut buttons, bytes, &mut i);
+		extract_joltage(&mut joltage, bytes, &mut i);
+	}
+
+	let buttons: &[u32] = &buttons[..];
+	let joltage: &[u32] = &joltage[..];
+	assert!(buttons.len() <= 15);
+	assert!(joltage.len() <= 15);
+
+	let mut answer = [0; 16];
+	for r in 0..joltage.len() {
+		answer[r] = joltage[r];
+	}
+	answer[15] = 0;
+
+	let mut button_matrix = [[0u32; 16]; 16];
+	for c in 0..buttons.len() {
+		let mut button = buttons[c];
+		let mut r = 0;
+		while button != 0 {
+			if button & 0b1 != 0 {
+				button_matrix[r][c] = 1;
+			}
+			r += 1;
+			button >>= 1;
+		}
+		button_matrix[15][c] = 1;
+	}
+	let button_matrix = button_matrix;
+
+	let mut max_presses_per_button = [0; 16];
+	for c in 0..buttons.len() {
+		let mut min = u32::MAX;
+		for r in 0..joltage.len() {
+			let value = joltage[r];
+			if value < min && button_matrix[r][c] != 0 {
+				min = value;
+			}
+		}
+		max_presses_per_button[c] = min;
+	}
+	let max_presses_per_button = max_presses_per_button;
+
+	loop {
+		answer[15] += 1;
+		let presses = max_presses_per_button;
+		// TODO iterate through all possible presses that sum up to answre[15]???
+		if is_correct(&button_matrix, &presses, &answer) {
+			return answer[15] as usize;
+		}
+		todo!()
+	}
+}
+
+#[track_caller]
+fn debug_print(name: &str, value: u32) {
+	if cfg!(debug_assertions) {
+		println!("[{}:{}] {name} = {value:08b}", file!(), line!());
+	}
+}
+
+fn extract_target(target: &mut u32, bytes: &[u8], i: &mut usize) {
+	let mut target_setter = 1;
+	loop {
+		match bytes[*i] {
+			b'#' => {
+				*target |= target_setter;
+				target_setter <<= 1;
+			}
+			b'.' => {
+				target_setter <<= 1;
+			}
+			b']' => {
+				*i += 3;
+				break;
+			}
+			_ => (),
+		}
+		*i += 1;
+	}
+}
+fn extract_buttons(buttons: &mut SmallVec<[u32; 16]>, bytes: &[u8], i: &mut usize) {
+	let mut button = 0;
+	let mut number = 0;
+	loop {
+		let b = bytes[*i];
+		*i += 1;
+		match b {
+			b'0'..=b'9' => {
+				number *= 10;
+				number += u32::from(b - b'0');
+			}
+			b',' => {
+				button |= 0b1 << number;
+				number = 0;
+			}
+			b')' => {
+				button |= 0b1 << number;
+				number = 0;
+				buttons.push(button);
+				button = 0;
+			}
+			b'{' => return,
+			_ => continue,
+		}
+	}
+}
+
+fn extract_joltage(joltage: &mut SmallVec<[u32; 16]>, bytes: &[u8], i: &mut usize) {
+	let mut number = 0;
+	loop {
+		let b = bytes[*i];
+		*i += 1;
+		match b {
+			b'0'..=b'9' => {
+				number *= 10;
+				number += u32::from(b - b'0');
+			}
+			b',' => {
+				joltage.push(number);
+				number = 0;
+			}
+			b'}' => {
+				joltage.push(number);
+				return;
+			}
+			_ => continue,
+		}
+	}
+}
+
+fn is_correct(matrix: &[[u32; 16]; 16], vector: &[u32; 16], answer: &[u32; 16]) -> bool {
+	let mut result = [0; 16];
+	for r in 0..16 {
+		for c in 0..16 {
+			result[r] += matrix[r][c] * vector[c];
+			if result[r] != answer[r] {
+				return false;
+			}
+		}
+	}
+	true
 }
 
 #[cfg(test)]
@@ -125,9 +237,17 @@ mod tests {
 	fn test_part2_given() {
 		init_logger();
 
-		let given = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-            [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-            [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}";
-		assert_eq!(part2(given), 24);
+		assert_eq!(
+			assess_part2("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"),
+			10
+		);
+		assert_eq!(
+			assess_part2("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"),
+			12
+		);
+		assert_eq!(
+			assess_part2("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"),
+			11
+		);
 	}
 }
